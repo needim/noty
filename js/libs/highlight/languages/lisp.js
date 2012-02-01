79c18eb44561bdbc2a8230121cf1a6995c4eea79
Nedim Arabaci <nedimarabaci@gmail.com>
2012-02-01 17:29:22 +0200

    Documentation

---
+++ js/libs/highlight/languages/lisp.js
@@ -0,0 +1,87 @@
+/*
+Language: Lisp
+Description: Generic lisp syntax
+Author: Vasily Polovnyov <vast@whiteants.net>
+*/
+
+hljs.LANGUAGES.lisp = function(){
+  var LISP_IDENT_RE = '[a-zA-Z_\\-\\+\\*\\/\\<\\=\\>\\&\\#][a-zA-Z0-9_\\-\\+\\*\\/\\<\\=\\>\\&\\#]*';
+  var LISP_SIMPLE_NUMBER_RE = '(\\-|\\+)?\\d+(\\.\\d+|\\/\\d+)?((d|e|f|l|s)(\\+|\\-)?\\d+)?';
+  var LITERAL = {
+    className: 'literal',
+    begin: '\\b(t{1}|nil)\\b'
+  };
+  var NUMBER1 = {
+    className: 'number', begin: LISP_SIMPLE_NUMBER_RE
+  };
+  var NUMBER2 = {
+    className: 'number', begin: '#b[0-1]+(/[0-1]+)?'
+  };
+  var NUMBER3 = {
+    className: 'number', begin: '#o[0-7]+(/[0-7]+)?'
+  };
+  var NUMBER4 = {
+    className: 'number', begin: '#x[0-9a-f]+(/[0-9a-f]+)?'
+  };
+  var NUMBER5 = {
+    className: 'number', begin: '#c\\(' + LISP_SIMPLE_NUMBER_RE + ' +' + LISP_SIMPLE_NUMBER_RE, end: '\\)'
+  };
+  var STRING = {
+    className: 'string',
+    begin: '"', end: '"',
+    contains: [hljs.BACKSLASH_ESCAPE],
+    relevance: 0
+  };
+  var COMMENT = {
+    className: 'comment',
+    begin: ';', end: '$'
+  };
+  var VARIABLE = {
+    className: 'variable',
+    begin: '\\*', end: '\\*'
+  };
+  var KEYWORD = {
+    className: 'keyword',
+    begin: '[:&]' + LISP_IDENT_RE
+  };
+  var QUOTED_LIST = {
+    begin: '\\(', end: '\\)'
+  };
+  QUOTED_LIST.contains = [QUOTED_LIST, LITERAL, NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5, STRING];
+  var QUOTED1 = {
+    className: 'quoted',
+    begin: '[\'`]\\(', end: '\\)',
+    contains: [NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5, STRING, VARIABLE, KEYWORD, QUOTED_LIST]
+  };
+  var QUOTED2 = {
+    className: 'quoted',
+    begin: '\\(quote ', end: '\\)',
+    keywords: {'title': {'quote': 1}},
+    contains: [NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5, STRING, VARIABLE, KEYWORD, QUOTED_LIST]
+  };
+  var LIST = {
+    className: 'list',
+    begin: '\\(', end: '\\)'
+  };
+  var BODY = {
+    className: 'body',
+    endsWithParent: true, excludeEnd: true
+  };
+  LIST.contains = [{className: 'title', begin: LISP_IDENT_RE}, BODY];
+  BODY.contains = [QUOTED1, QUOTED2, LIST, LITERAL, NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5, STRING, COMMENT, VARIABLE, KEYWORD];
+
+  return {
+    case_insensitive: true,
+    defaultMode: {
+      illegal: '[^\\s]',
+      contains: [
+        LITERAL,
+        NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5,
+        STRING,
+        COMMENT,
+        QUOTED1, QUOTED2,
+        LIST
+      ]
+    }
+  };
+}();
---
+++ js/libs/highlight/languages/parser3.js
@@ -0,0 +1,52 @@
+/*
+Language: Parser3
+Requires: xml.js
+Author: Oleg Volchkov <oleg@volchkov.net>
+*/
+
+hljs.LANGUAGES.parser3 = function() {
+  var COMMENTED_BLOCK = {
+    begin: '{', end: '}'
+  };
+  COMMENTED_BLOCK.contains = [COMMENTED_BLOCK];
+
+  return {
+    defaultMode: {
+      subLanguage: 'html',
+      contains: [
+        {
+          className: 'comment',
+          begin: '^#', end: '$'
+        },
+        {
+          className: 'comment',
+          contains: [COMMENTED_BLOCK],
+          begin: '\\^rem{', end: '}',
+          relevance: 10
+        },
+        {
+          className: 'preprocessor',
+          begin: '^@(?:BASE|USE|CLASS|OPTIONS)$',
+          relevance: 10
+        },
+        {
+          className: 'title',
+          begin: '@[\\w\\-]+\\[[\\w^;\\-]*\\](?:\\[[\\w^;\\-]*\\])?(?:.*)$'
+        },
+        {
+          className: 'variable',
+          begin: '\\$\\{?[\\w\\-\\.\\:]+\\}?'
+        },
+        {
+          className: 'keyword',
+          begin: '\\^[\\w\\-\\.\\:]+'
+        },
+        {
+          className: 'number',
+          begin: '\\^#[0-9a-fA-F]+'
+        },
+        hljs.C_NUMBER_MODE
+      ]
+    }
+  };
+}();
