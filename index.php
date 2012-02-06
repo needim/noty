1f485b3f6d6536c79b44a71449ee4604de44333f
Nedim Arabaci <nedimarabaci@gmail.com>
2012-02-06 17:21:23 +0200

    Github commit history added to documentation

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
   <link rel="stylesheet" href="css/buttons.css">
   <link rel="stylesheet" href="js/noty/css/jquery.noty.css">
   <link rel="stylesheet" href="js/libs/highlight/styles/dark.css">
-  
+
   <!-- end CSS-->
-  
+
   <link href='http://fonts.googleapis.com/css?family=PT+Sans:regular,italic,bold,bolditalic&amp;subset=latin,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
 
   <script src="js/libs/modernizr-2.0.6.min.js"></script>
@@ -30,14 +30,14 @@
 
   <div id="container">
     <header>
-			
+
 			<a href="/noty" title="noty - a jquery notification plugin">
 				<img src="img/noty_head.png" alt="noty - a jquery notification plugin" />
 			</a>
-			
+
     </header>
     <div id="main" role="main">
-    
+
     	<div id="noty_queue">
     		<span>noty queue</span>
     		<ul id="noty_queue_list">
@@ -45,7 +45,7 @@
     	</div>
 
 			<ul id="examples">
-			
+
 				<li>
 					<img src="img/noty_example.png" alt="Example 1" />
 					<ul class="sub-list ex1">
@@ -96,38 +96,38 @@
 						<li class="confirm ex5">Confirm</li>
 					</ul>
 				</li>
-				
+
 			</ul>
-			
+
 			<div id="fork-me">
-					
+
 				<a href="https://github.com/needim/noty">
 					<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/bec6c51521dcc8148146135149fe06a9cc737577/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub">
 				</a>
-			
+
 			</div>
-			
+
     </div>
-    
+
     <div id="documentation">
-			
+
 			<h1>Documentation</h1>
-			
+
 			<div id="usage">
-			
+
 				<section role="installation">
-					
+
 					<h3>Installation</h3>
-					
+
 					<div class="description">
-					
+
 						Include <a href="http://docs.jquery.com/Downloading_jQuery#Current_Release">jQuery latest release</a> in your header. The Google AJAX Libraries API can be used for this but you could also host the library yourself.
 
 						<p>Download <span class="hl">noty</span> from the <a href="https://github.com/needim/noty/downloads">Github</a> and upload the files to your server.</p>
 						Include <span class="hl">jquery.noty.js</span> and its dependancies below jQuery.
-						Also add <span class="hl">jquery.noty.css</span> to your header. 
+						Also add <span class="hl">jquery.noty.css</span> to your header.
 						Your code should then be similar to this:
-						
+
 						<pre>
 						<code class="html">
 &lt;script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"&gt;&lt;/script&gt;
@@ -136,19 +136,19 @@
 &lt;link rel="stylesheet" type="text/css" href="css/jquery.noty.css"/&gt;
 						</code>
 						</pre>
-						
+
 					</div>
-				
+
 				</section>
-				
+
 				<section role="creating-noty">
-					
+
 					<h3>Creating a noty</h3>
-					
+
 					<div class="description">
-					
+
 						We wrote a helper for creating noty easily. So you can use <span class="hl">noty(properties);</span> Just like this;
-						
+
 						<pre>
 						<code>
 // ex1 - alert
@@ -157,19 +157,19 @@ $('.ex1.alert').click(function() {
 });
 						</code>
 						</pre>
-					
+
 					</div>
-				
+
 				</section>
-				
+
 				<section role="options">
-					
+
 					<h3>Noty Options</h3>
-					
+
 					<div class="description">
-					
+
 						Available options listed below.
-						
+
 						<table>
 							<tr>
 								<th>Name</th>
@@ -262,25 +262,25 @@ $('.ex1.alert').click(function() {
 								<td><a href="#callbacks">onClose Callback &raquo;</a></td>
 							</tr>
 						</table>
-						
+
 					</div>
-				
+
 				</section>
-				
+
 				<section role="buttons">
-					
+
 					<h3 id="buttons">Buttons</h3>
-					
+
 					<div class="description">
-					
+
 						We can set button objects with an array like above;
-						
+
 						<pre>
 						<code>
 // ex1 - confirm
 $('.ex1.confirm').click(function() {
 	noty({
-		text: 'noty - a jquery notification library!', 
+		text: 'noty - a jquery notification library!',
 		buttons: [
 			{type: 'button green', text: 'Ok', click: function() { /**/ } },
 			{type: 'button pink', text: 'Cancel', click: function() { /**/ } }
@@ -291,9 +291,9 @@ $('.ex1.confirm').click(function() {
 });
 						</code>
 						</pre>
-						
+
 						Available button properties are listed below.
-						
+
 						<table>
 							<tr>
 								<th>Name</th>
@@ -316,17 +316,17 @@ $('.ex1.confirm').click(function() {
 								<td>Callback function of button</td>
 							</tr>
 						</table>
-						
+
 					</div>
-				
+
 				</section>
-				
+
 				<section role="callbacks">
-					
+
 					<h3 id="callbacks">Callbacks</h3>
-					
+
 					<div class="description">
-					
+
 						Noty plugin have 2 callback option for now. We will add more callback asap.
 
 						<table>
@@ -343,17 +343,17 @@ $('.ex1.confirm').click(function() {
 								<td>Triggered when notification close animation finished.</td>
 							</tr>
 						</table>
-					
+
 					</div>
-				
+
 				</section>
-				
+
 				<section role="api">
-					
+
 					<h3>Noty API</h3>
-					
+
 					<div class="description">
-					
+
 						<table>
 							<tr>
 								<th>Function</th>
@@ -376,15 +376,59 @@ $('.ex1.confirm').click(function() {
 								<td>Close all notifications. (<a class="api-func" title="$.noty.closeAll();" href="">run</a>)</td>
 							</tr>
 						</table>
-						
+
 					</div>
-				
+
 				</section>
-				
+
+				<section role="commit-history">
+
+					<h3>Github Commit History</h3>
+
+					<div class="description">
+
+						<?php
+								$ch = curl_init();
+
+								curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/needim/noty/commits');
+								curl_setopt($ch, CURLOPT_HEADER, 0);
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+								curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+								curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
+
+								$response = json_decode(curl_exec($ch));
+								curl_close($ch);
+						?>
+
+						<table>
+							<tr>
+								<th>Committer</th>
+								<th>Message</th>
+								<th class="default">Date</th>
+								<th>Details</th>
+							</tr>
+
+							<?php foreach($response as $commit): ?>
+
+								<tr>
+									<td><?php echo $commit->commit->author->name ?></td>
+									<td><?php echo $commit->commit->message ?></td>
+									<td><?php echo date('d.m.Y H:i', strtotime($commit->commit->author->date)); ?></td>
+									<td><a href="https://github.com/needim/noty/commit/<?php echo $commit->sha ?>" title="Commit Details">view &raquo</a></td>
+								</tr>
+
+							<?php endforeach; ?>
+
+						</table>
+
+					</div>
+
+				</section>
+
 			</div>
-		
+
 		</div>
-    
+
     <footer>
 
     </footer>
@@ -401,23 +445,23 @@ $('.ex1.confirm').click(function() {
 	  hljs.tabReplace = '  ';
 	  hljs.initHighlightingOnLoad();
   </script>
-  
+
   <script defer src="js/noty/js/jquery.noty.js"></script>
-  
+
   <!-- end scripts-->
 
 	<script type="text/javascript">
-	
+
 	  var _gaq = _gaq || [];
 	  _gaq.push(['_setAccount', 'UA-28737816-1']);
 	  _gaq.push(['_trackPageview']);
-	
+
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
-	
+
 	</script>
 
 
@@ -425,6 +469,6 @@ $('.ex1.confirm').click(function() {
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
-  
+
 </body>
 </html>
