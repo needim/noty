167dfb961f290a4f478470783883e79d2df0cfa2
Loic Knuchel <loicknuchel@gmail.com>
2012-05-19 11:56:06 +0200

    add demo to view styles

---
+++ demo/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  //alert('toto');
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'message',      text: 'message with default theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'warning',      text: 'warning with default theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'alert',        text: 'alert with default theme',        timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'information',  text: 'information with default theme',  timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'success',      text: 'success with default theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_default', type : 'error',        text: 'error with default theme',        timeout : false });
+  
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'message',      text: 'message with twitter theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'warning',      text: 'warning with twitter theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'alert',        text: 'alert with twitter theme',        timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'information',  text: 'information with twitter theme',  timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'success',      text: 'success with twitter theme',      timeout : false });
+  noty({layout : 'topLeft', theme : 'noty_theme_twitter', type : 'error',        text: 'error with twitter theme',        timeout : false });
+  
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'message',      text: 'message with mitgux theme',      timeout : false });
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'warning',      text: 'warning with mitgux theme',      timeout : false });
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'alert',        text: 'alert with mitgux theme',        timeout : false });
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'information',  text: 'information with mitgux theme',  timeout : false });
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'success',      text: 'success with mitgux theme',      timeout : false });
+  noty({layout : 'topRight', theme : 'noty_theme_mitgux', type : 'error',        text: 'error with mitgux theme',        timeout : false });
+  
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'message',      text: 'message with facebook theme',      timeout : false });
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'warning',      text: 'warning with facebook theme',      timeout : false });
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'alert',        text: 'alert with facebook theme',        timeout : false });
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'information',  text: 'information with facebook theme',  timeout : false });
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'success',      text: 'success with facebook theme',      timeout : false });
+  noty({layout : 'bottomLeft', theme : 'noty_theme_facebook', type : 'error',        text: 'error with facebook theme',        timeout : false });
+  
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'message',      text: 'message with desktop theme',      timeout : false });
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'warning',      text: 'warning with desktop theme',      timeout : false });
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'alert',        text: 'alert with desktop theme',        timeout : false });
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'information',  text: 'information with desktop theme',  timeout : false });
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'success',      text: 'success with desktop theme',      timeout : false });
+  noty({layout : 'bottomRight', theme : 'noty_theme_desktop', type : 'error',        text: 'error with desktop theme',        timeout : false });
+});
