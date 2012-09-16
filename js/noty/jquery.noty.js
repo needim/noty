6ff415d3b72511d6466c6e246eca153a0cfbc0bb
Clemens <clemens@heypanda.de>
2012-09-16 02:44:13 +0200

    fixed bug where queue was modified while iterating over it which caused 'undefined' access

--- js/noty/jquery.noty.js
+++ js/noty/jquery.noty.js
@@ -134,7 +134,7 @@ if (typeof Object.create !== 'function') {
 			var self = this;
 
 			if (!this.shown) { // If we are still waiting in the queue just delete from queue
-				$.each($.noty.queue, function(i, n) {
+				$.each($.noty.queue.slice(), function(i, n) {
 					if (n.options.id == self.options.id) {
 						$.noty.queue.splice(i, 1);
 					}
