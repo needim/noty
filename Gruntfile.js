01d4e74500e01f5b712440c2c5b8793cc989c77f
Nedim Arabacı <nedimarabaci@gmail.com>
2014-05-22 10:50:07 +0300

    Refactoring! Use 2 space instead of tab char.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,47 @@
-module.exports = function (grunt) {
-	grunt.initConfig({
+module.exports = function(grunt) {
+    grunt.initConfig({
 
-        bump: {
+        bump  : {
             options: {
-                files: ['package.json', 'noty.jquery.json', 'bower.json', 'js/noty/jquery.noty.js'],
-                updateConfigs: [],
-                commit: false,
-                commitMessage: 'Release v%VERSION%',
-                commitFiles: ['-a'],
-                createTag: true,
-                tagName: 'v%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: false,
-                pushTo: 'upstream',
+                files             : ['package.json', 'noty.jquery.json', 'bower.json', 'js/noty/jquery.noty.js'],
+                updateConfigs     : [],
+                commit            : false,
+                commitMessage     : 'Release v%VERSION%',
+                commitFiles       : ['-a'],
+                createTag         : true,
+                tagName           : 'v%VERSION%',
+                tagMessage        : 'Version %VERSION%',
+                push              : false,
+                pushTo            : 'upstream',
                 gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
             }
         },
-		concat: {
-			dist: {
-				src: ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js'],
-				dest: 'js/noty/packaged/jquery.noty.packaged.js'
-			}
-		},
+        concat: {
+            dist: {
+                src : ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js'],
+                dest: 'js/noty/packaged/jquery.noty.packaged.js'
+            }
+        },
 
-		uglify: {
-            options: {
+        uglify: {
+            options : {
                 preserveComments: function(a) {
                     return !!(a.start.file == 'js/noty/jquery.noty.js' && a.start.line == 11);
                 }
             },
-			minifyJS: {
-				files: {
-					'js/noty/packaged/jquery.noty.packaged.min.js': ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js']
-				}
-			}
-		}
-	});
+            minifyJS: {
+                files: {
+                    'js/noty/packaged/jquery.noty.packaged.min.js': ['js/noty/jquery.noty.js', 'js/noty/layouts/*.js', 'js/noty/themes/*.js']
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-bump');
 
-	grunt.registerTask('build', ['bump', 'concat', 'uglify:minifyJS']);
-	grunt.registerTask('conc', ['concat']);
-	grunt.registerTask('ugly', ['uglify:minifyJS']);
+    grunt.registerTask('build', ['bump', 'concat', 'uglify:minifyJS']);
+    grunt.registerTask('conc', ['concat']);
+    grunt.registerTask('ugly', ['uglify:minifyJS']);
 };
