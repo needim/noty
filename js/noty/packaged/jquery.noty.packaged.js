ff9f40445c0c1e2d53fe818e84b9241768952a69
Nedim Arabacı <nedimarabaci@gmail.com>
2014-09-22 13:54:32 +0300

    version 2.2.7

--- js/noty/packaged/jquery.noty.packaged.js
+++ js/noty/packaged/jquery.noty.packaged.js
@@ -1,6 +1,6 @@
 /*!
  @package noty - jQuery Notification Plugin
- @version version: 2.2.5
+ @version version: 2.2.7
  @contributors https://github.com/needim/noty/graphs/contributors
 
  @documentation Examples and Documentation - http://needim.github.com/noty/
@@ -1032,6 +1032,60 @@ window.noty = function noty(options) {
     };
 
 })(jQuery);
+(function($) {
+
+	$.noty.themes.bootstrapTheme = {
+		name: 'bootstrapTheme',
+		modal: {
+			css: {
+				position: 'fixed',
+				width: '100%',
+				height: '100%',
+				backgroundColor: '#000',
+				zIndex: 10000,
+				opacity: 0.6,
+				display: 'none',
+				left: 0,
+				top: 0
+			}
+		},
+		style: function() {
+		
+			var containerSelector = this.options.layout.container.selector;
+			$(containerSelector).addClass('list-group');
+			
+			this.$closeButton.append('<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>');
+			this.$closeButton.addClass('close');
+		
+			this.$bar.addClass( "list-group-item" );
+
+			switch (this.options.type) {
+				case 'alert': case 'notification':
+					this.$bar.addClass( "list-group-item-info" ); 
+					break;
+				case 'warning':
+					this.$bar.addClass( "list-group-item-warning" );
+					break;
+				case 'error':
+					this.$bar.addClass( "list-group-item-danger" );
+					break;
+				case 'information':
+					this.$bar.addClass("list-group-item-info");
+					break;
+				case 'success':
+					this.$bar.addClass( "list-group-item-success" );
+					break;
+			}
+		},
+		callback: {
+			onShow: function() {  },
+			onClose: function() {  }
+		}
+	};
+
+})(jQuery);
+
+
 (function($) {
 
     $.noty.themes.defaultTheme = {
