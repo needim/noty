8187ad662dca8075fa7b2f422a33274298b6e841
Nedim Arabacı <nedimarabaci@gmail.com>
2014-11-20 14:00:27 +0200

    sharp char fix

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ window.anim.close = 'flipOutX';
 
 		new Share(".sharer-btn", {
 			title: 'NOTY - a jQuery Notification Plugin',
-			description: '#notyjs is a jQuery plugin that makes it easy to create alert - success - error - warning - information - confirmation messages as an alternative the standard alert dialog.',
+			description: '&num;notyjs is a jQuery plugin that makes it easy to create alert - success - error - warning - information - confirmation messages as an alternative the standard alert dialog.',
 			image: 'http://ned.im/noty/images/projects/noty-v2-logo.png',
 			networks: {
 				pinterest: {
